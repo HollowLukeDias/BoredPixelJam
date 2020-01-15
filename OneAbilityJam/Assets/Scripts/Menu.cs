1c01e25e2a1036c152a40a457b5fa8f6bce197e5
Luke Dias <lhollowwizard@gmail.com>
2020-01-15 06:07:37 -0300

    Gitignore not working properly, but, well, shit happens, just use important stuff

---
+++ OneAbilityJam/Assets/Scripts/Menu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    [SerializeField] private GameObject panel;
+    private bool isPaused = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                Pause(panel);
+            } else if (isPaused)
+            {
+                Unpause(panel);
+            }   
+        }
+    }
+    
+    public void StartGame(string sceneName)
+    {
+        FindObjectOfType<Fader>().FadeTo(sceneName);
+    }
+
+    public void OptionsMenu()
+    {
+        //TODO: Make another canvas with the sound options and credits
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Pause(GameObject panel)
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+        panel.SetActive(true);
+    }
+    public void Unpause(GameObject panel)
+    {
+        isPaused = false;
+        Time.timeScale = 1;
+        panel.SetActive(false);
+    }
+}
---
+++ OneAbilityJam/Assets/SinMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinMove : MonoBehaviour
+{
+    void Update()
+    {
+        StartCoroutine(RandomMover());
+    }
+
+    IEnumerator RandomMover()
+    {
+        var t = 0f;
+        while (true)
+        {
+            transform.position = new Vector2(Mathf.Sin(t), 0);
+            t += Time.deltaTime;
+            yield return 0;
+        }
+    }
+}
